63fedef17c1972946555bd32decbb150ee5fd7ba
FcoRenato <fco.renatogomes@gmail.com>
2025-09-21 17:13:57 -0300

    wip

--- api/v1/classes/Adp9.php
+++ api/v1/classes/Adp9.php
@@ -11,6 +11,8 @@ class Adp9
         $props = explode('-', $parametros);
         $datetime_last_reg= $props[0];
 
+        echo 'data e hora: ' . $datetime_last_reg . '<hr>';
+
         // 5 - dados vendas geral para CLUBE G7
         $sql4 = "SELECT v.data_cupom,
                         se.codigo              AS cod_empresa,
@@ -48,7 +50,8 @@ class Adp9
                 ORDER BY v.cnpj_cpf_rodape,  se.codigo, v.numero_cupom, Date(data_cupom)
         ";
 
-
+        echo $sql4 . '<hr>';
+        
         $sql = $con->prepare($sql4);
         $sql->execute();
         //print_r($sql->errorInfo());
