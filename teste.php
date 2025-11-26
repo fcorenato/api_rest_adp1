bac09919181e974c891cf0ed94542c3fbd48da2b
FcoRenato <fco.renatogomes@gmail.com>
2025-11-26 10:32:46 -0300

    wip timeout

--- teste.php
+++ teste.php
@@ -6,10 +6,32 @@ error_reporting(E_ALL);
 ini_set('default_socket_timeout', 300);
 ini_set('max_execution_time', 300);
 
-require 'v1/classes/Adp_conect.php';
+
+// $timeout = 300;
+
+// Configurações de conexão com o banco de dados
+
+$host = "10.226.0.1";
+$port = "5432";
+$dbname = "g7carioca_db";
+$username = "g7carioca_ro";
+$password = "d2ss1efnjerk";
+
+// $con = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
+
+
+$timeout = 300;
+
+$con = new PDO(
+    "pgsql:host=$host;port=$port;dbname=$dbname",
+    $username,
+    $password,
+    [
+        PDO::ATTR_TIMEOUT => $timeout,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]
+);
 
 $q = $con->prepare("SELECT pg_sleep(120)");
 $q->execute();
 echo "OK";
-
-?>
