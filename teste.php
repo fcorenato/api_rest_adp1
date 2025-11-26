18ce3eed894c164c83da1ebcc4e39ce7e29902f9
FcoRenato <fco.renatogomes@gmail.com>
2025-11-26 10:14:36 -0300

    arquivo teste pdo

--- teste.php
+++ teste.php
@@ -1,7 +1,9 @@
 <?php
+ini_set('default_socket_timeout', 300);
 ini_set('max_execution_time', 300);
-sleep(120);
+require 'Adp_conect.php';
+
+$q = $pdo->prepare("SELECT SLEEP(120)");
+$q->execute();
 echo "OK";
-
-
 ?>
