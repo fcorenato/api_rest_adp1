2a86d889208d4cffce9447e207300c68a57fcb1c
FcoRenato <fco.renatogomes@gmail.com>
2025-11-26 10:31:28 -0300

    wip timeout

--- teste.php
+++ teste.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 ini_set('default_socket_timeout', 300);
 ini_set('max_execution_time', 300);
 
-require 'Adp_conect.php';
+require 'v1/classes/Adp_conect.php';
 
 $q = $con->prepare("SELECT pg_sleep(120)");
 $q->execute();
