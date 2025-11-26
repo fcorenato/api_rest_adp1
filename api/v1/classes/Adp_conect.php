fd1fa5bee1b8882c1744cd4f205d151e111732ba
FcoRenato <fco.renatogomes@gmail.com>
2025-11-26 11:13:28 -0300

    wip timeout

--- api/v1/classes/Adp_conect.php
+++ api/v1/classes/Adp_conect.php
@@ -1,6 +1,11 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 ini_set('default_socket_timeout', 300);
 ini_set('max_execution_time', 300);
+
 
 // $timeout = 300;
 
