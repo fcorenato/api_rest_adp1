9f99635ab6ee8464594f2bc778dc82ef1c19552f
FcoRenato <fco.renatogomes@gmail.com>
2026-07-13 23:57:01 -0300

    finacasa conexao

--- api/finacasa/src/cron/md_vendas_rel_carteira_preproc_ajax_v3.php
+++ api/finacasa/src/cron/md_vendas_rel_carteira_preproc_ajax_v3.php
@@ -81,11 +81,11 @@ if ($processar_carteira) {
     } else {
         require('../config/conexao.php');
         //criar array com dados do produto:
-        echo 'Criar array com dados do produto:';
+        echo 'Criar array com dados do produto:<br>';
         $query1 = "SELECT * FROM md_cad_produtos";
         $result_query1 = mysql_query($query1);
         $qtde_query1 = mysql_num_rows($result_query1);
-        echo "qtde_query1: " . $qtde_query1;
+        echo "qtde_query1: " . $qtde_query1 . "<br>";
         if ($qtde_query1 > 0) {
             while ($campos = mysql_fetch_array($result_query1)) {
                 $prod_array_ipi[trim($campos['referencia'])] = $campos['ipi'];
