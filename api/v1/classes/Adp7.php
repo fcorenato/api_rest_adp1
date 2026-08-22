35d51012e8241132eb1190ba0e6e387292a64b4f
FcoRenato <fco.renatogomes@gmail.com>
2026-08-22 17:30:23 -0300

    pesquisa cupom caminhoneiro

--- api/v1/classes/Adp7.php
+++ api/v1/classes/Adp7.php
@@ -44,13 +44,14 @@ FROM   item_venda_cf iv
                 ON mvt.id_empresa = se.id_empresa
         INNER JOIN categoria_item ci
                 ON ci.id_categoria_item = i.id_categoria_item
-WHERE  se.codigo='$emp'
+WHERE  se.codigo in ('001','008')
        AND v.cancelada = 'N'
        AND IV.cancelado = 'N'
        AND (i.denominacao_reduzida = 'S10 AD' OR i.denominacao_reduzida = 'S10AD' OR i.denominacao_reduzida = 'DS10')
 --        AND v.data_cupom >= CURRENT_DATE - INTERVAL '30 days'
        AND v.numero_cupom = '$numnf' 
             GROUP BY v.data_cupom, se.codigo, se.nome, v.numero_cupom, p.codigo, p.fantasia_codinome,  pp.codigo, pp.nome, i.codigo, i.denominacao, v.checksum, i.denominacao_reduzida, ci.codigo
+            ORDER BY se.nome, v.numero_cupom
         ";
 
 
